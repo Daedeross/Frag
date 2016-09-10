0b09aa4bba0ea1800dd394de88f92425dbc8c800
Bryan C. Jones <daedeross@gmail.com>
2016-09-10 07:41:40 -0500

    First commit

---
+++ Assets/CameraControl.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraControl : MonoBehaviour {
+
+    public GameObject Orbit;
+    public GameObject Tilt;
+    public float OrbitSpeed = 400f;
+    public float TiltSpeed = 200f;
+
+    // Use this for initialization
+    void Start () {
+    }
+    
+    // Update is called once per frame
+    void Update () {
+        if (Input.GetButton("Secondary"))
+        {
+            float dy = TiltSpeed * Input.GetAxis("Mouse Y");
+
+            if (Input.GetButton("Primary"))
+            {
+                transform.Translate(0, 0, dy);
+                var pos = transform.position;
+                if (pos.z > 0)
+                {
+                    pos.z = 0;
+                    transform.position = pos;
+                }
+            }
+            else
+            {
+                float dx = OrbitSpeed * Input.GetAxis("Mouse X");
+                Orbit.transform.Rotate(0, dx, 0);
+                Tilt.transform.Rotate(dy, 0, 0);
+            }
+        }
+
+        Vector3 translate = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        Orbit.transform.Translate(translate);
+    }
+}
